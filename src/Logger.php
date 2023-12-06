2773d8135a898aff3a5bd26c430bc5f731107533
flavius.popoiu <flavius.popoiu@zitec.com>
2023-12-06 15:51:56 +0200

    Implemented first use case and tests

--- src/Logger.php
+++ src/Logger.php
@@ -6,8 +6,52 @@ namespace PFlav\PHPLogger;
 
 class Logger
 {
-    public function log(string $message): void
+    const LOG_LEVEL_INFO = 'Info';
+    const LOG_LEVEL_DEBUG = 'Debug';
+    const LOG_LEVEL_WARNING = 'Warning';
+    const LOG_LEVEL_CRITICAL = 'Critical';
+    private string $logLevel = self::LOG_LEVEL_DEBUG;
+    private array $logLevels = [
+        self::LOG_LEVEL_DEBUG => 1,
+        self::LOG_LEVEL_INFO => 2,
+        self::LOG_LEVEL_WARNING => 3,
+        self::LOG_LEVEL_CRITICAL => 4
+    ];
+    private function log(string $message, string $logLevel): void
     {
+        if ($this->logLevels[$logLevel] < $this->logLevels[$this->getLogLevel()]) {
+            return;
+        }
         echo $message . "\n";
+    }
+
+    public function debug(string $message): void
+    {
+        $this->log('Debug: ' . $message, self::LOG_LEVEL_DEBUG);
+    }
+
+    public function info(string $message): void
+    {
+        $this->log('Info: ' . $message, self::LOG_LEVEL_INFO);
+    }
+
+    public function warning(string $message): void
+    {
+        $this->log('Warning: ' . $message, self::LOG_LEVEL_WARNING);
+    }
+
+    public function critical(string $message): void
+    {
+        $this->log('Critical: ' . $message, self::LOG_LEVEL_CRITICAL);
+    }
+    
+    public function setLogLevel(string $logLevel): void
+    {
+        $this->logLevel = $logLevel;
+    }
+
+    public function getLogLevel(): string
+    {
+        return $this->logLevel;
     }
 }
--- tests/LoggerTest.php
+++ tests/LoggerTest.php
@@ -1,17 +1,77 @@
 <?php
 
+declare(strict_types=1);
+
 use PFlav\PHPLogger\Logger;
 use PHPUnit\Framework\TestCase;
 
 class LoggerTest extends TestCase
 {
-    /** @test */
-    public function a_logger_echoes_a_message_in_the_console()
+    private Logger $logger;
+    protected function setUp(): void
     {
-        $logger = new Logger();
+        parent::setUp();
+        $this->logger = new Logger();
+    }
+    private function getDebugOutput(): string
+    {
         ob_start();
-        $logger->log("Hello World");
-        $output = ob_get_clean();
-        $this->assertEquals("Hello World\n", $output);
+        $this->logger->debug("Hello World");
+        return ob_get_clean();
+    }
+
+    private function getInfoOutput(): string
+    {
+        ob_start();
+        $this->logger->info("Hello World");
+        return ob_get_clean();
+    }
+
+    private function getWarningOutput(): string
+    {
+        ob_start();
+        $this->logger->warning("Hello World");
+        return ob_get_clean();
+    }
+
+    private function getCriticalOutput(): string
+    {
+        ob_start();
+        $this->logger->critical("Hello World");
+        return ob_get_clean();
+    }
+
+    /** @test */
+    public function a_logger_logs_a_debug_message_to_the_console()
+    {
+        $this->assertEquals("Debug: Hello World\n", $this->getDebugOutput());
+    }
+
+    /** @test */
+    public function a_logger_logs_an_info_message_to_the_console()
+    {
+        $this->assertEquals("Info: Hello World\n", $this->getInfoOutput());
+    }
+
+    /** @test */
+    public function a_logger_logs_a_warning_message_to_the_console()
+    {
+        $this->assertEquals("Warning: Hello World\n", $this->getWarningOutput());
+    }
+
+    /** @test */
+    public function a_logger_logs_an_error_message_to_the_console()
+    {
+        $this->assertEquals("Critical: Hello World\n", $this->getCriticalOutput());
+    }
+
+    /** @test */
+    public function a_debug_message_is_not_logged_if_the_log_level_is_set_to_info_but_info_warning_and_error_messages_are()
+    {
+        $this->logger->setLogLevel(Logger::LOG_LEVEL_INFO);
+        $this->assertEquals("", $this->getDebugOutput());
+        $this->assertEquals("Info: Hello World\n", $this->getInfoOutput());
+        $this->assertEquals("Warning: Hello World\n", $this->getWarningOutput());
+        $this->assertEquals("Critical: Hello World\n", $this->getCriticalOutput());
     }
 }
